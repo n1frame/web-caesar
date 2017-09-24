4031a128304683fd0ce5cd535ba8c5866572c7bc
Collin Elliott <collinelliott@gmail.com>
2017-09-24 16:32:29 -0500

    Syntax for web-caesar complete

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app=Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+
+    <body>
+        <form action ="/encrypt" method ="POST">
+        <label for="rot">Rotate by: </label>
+        <input name="rot" type="text" value="0"></input>
+        <textarea name="text"> {0} </textarea>
+        <input type="Submit"></input>
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/encrypt", methods=['POST'])
+def encrypt():
+    text= request.form['text']
+    rot= request.form['rot']
+    rot= int(rot)
+
+    encrypt_text = rotate_string (text, rot)
+
+    return form.format(encrypt_text)
+
+@app.route("/")
+def index():
+    return form.format("")
+
+app.run()
